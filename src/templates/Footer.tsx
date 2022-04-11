8df55bedce17adcf92aabcf39b1cc3574a00fb68
Derrint <lorensioderrint@gmail.com>
2022-04-11 22:47:57 +0700

    Footer : apply design

--- src/templates/Footer.tsx
+++ src/templates/Footer.tsx
@@ -1,8 +1,10 @@
+import Image from 'next/image';
 import Link from 'next/link';
 import { FaDiscord, FaTwitter } from 'react-icons/fa';
 
 import { Background } from '@components/background/Background';
 import { Section } from '@components/layout/Section';
+import LogoBW from '@images/logos/logo-sds-bw.png';
 
 const Footer = () => {
   const menus = [
@@ -32,19 +34,14 @@ const Footer = () => {
     <Background color="bg-white">
       <Section yPadding="py-8" isFooter>
         <div className="flex flex-wrap justify-between items-center">
-          <div className="w-full sm:w-7/12 lg:w-5/12">
-            <Link href="/">
-              <a className="bg-gradient-to-r from-gradient-primary-start to-gradient-primary-end bg-clip-text text-transparent font-bold text-xl">
-                vesselscan
-              </a>
-            </Link>
-            <p className="font-medium text-xs mt-2">
-              Vesselscan is a new blockchain built for the next generation of
-              apps, games, and digital assets.
+          <div className="w-full sm:w-6/12 flex items-center gap-10">
+            <Image src={LogoBW} alt="" height={48} width={190} />
+            <p className="text-sm uppercase">
+              Copyright © 2022 Simpleduckstudios
             </p>
           </div>
 
-          <nav className="w-full lg:w-7/12 mt-6 lg:mt-0">
+          <nav className="w-full sm:w-6/12 mt-6 lg:mt-0 hidden">
             <ul className="navbar flex flex-col sm:flex-row gap-4 sm:gap-8 lg:gap-10 justify-start lg:justify-end items-start sm:items-center font-medium text-xl text-gray-800">
               {menus.map((item: any) => {
                 return (
@@ -53,7 +50,7 @@ const Footer = () => {
                     className="flex justify-center items-center gap-3"
                   >
                     <Link href="/">
-                      <a className="font-bold text-sm">{item.label}</a>
+                      <a className="uppercase text-sm">{item.label}</a>
                     </Link>
                     {item.id === 4 && (
                       <>
