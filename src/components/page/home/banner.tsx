e3f415a0095554c7e00bd0cc5168d478e323f3db
Derrint <lorensioderrint@gmail.com>
2022-04-12 11:35:25 +0700

    Home - Banner : update font styling, add underline animation

--- src/components/page/home/banner.tsx
+++ src/components/page/home/banner.tsx
@@ -60,23 +60,80 @@ const Banner = () => {
   return (
     <Section>
       <div className="relative min-h-[720px] flex flex-col justify-center">
-        <h1 className="text-8xl">
+        <h1 className="text-8xl font-bold">
           {text?.first}{' '}
           <Fade left duration={750} cascade when={isTextShown}>
             <span
-              className={`text-secondary underline decoration-wavy underline-offset-8 decoration-1 transition ease-in-out duration-200 `}
+              className={`hover:text-secondary underlined underlined-waved transition-all duration-200`}
             >
               {text?.second}
             </span>
           </Fade>
         </h1>
-        <h2 className="text-6xl mt-5 mb-40">{text?.third}</h2>
+        <h2 className="text-6xl font-bold mt-8 mb-40">{text?.third}</h2>
         <img
           src="https://derrint.sirv.com/Images/simple-duck-studios/home/banner-phone.png"
           alt=""
           className="absolute right-0 top-1/2 h-[500px] -translate-y-1/2"
         />
       </div>
+
+      <style jsx>
+        {`
+          @keyframes movemask {
+            0% {
+              -webkit-mask-position: 0 0;
+              mask-position: 0 0;
+            }
+
+            to {
+              -webkit-mask-position: 114px 0;
+              mask-position: 114px 0;
+            }
+          }
+          .underlined {
+            position: relative;
+          }
+
+          .underlined:after,
+          .underlined:before {
+            position: absolute;
+            width: 100%;
+            height: 11px;
+            bottom: 10px;
+            left: 0;
+            background: #000;
+            content: '';
+          }
+          .underlined:after {
+            width: 0;
+            transition: all 0.6s;
+            background: #0083ff;
+          }
+
+          .underlined:before {
+            opacity: 0.2;
+          }
+
+          .underlined:hover:after,
+          .underlined:hover:before {
+            -webkit-animation: movemask 2s linear infinite;
+            animation: movemask 2s linear infinite;
+          }
+
+          .underlined:hover:after {
+            width: 100%;
+            transition-timing-function: cubic-bezier(0.2, 0, 0, 1);
+            background: #0083ff;
+          }
+          .underlined-waved:after,
+          .underlined-waved:before {
+            bottom: 0px !important;
+            -webkit-mask: url(https://derrint.sirv.com/Images/simple-duck-studios/home/underline-waved.svg);
+            mask: url(https://derrint.sirv.com/Images/simple-duck-studios/home/underline-waved.svg);
+          }
+        `}
+      </style>
     </Section>
   );
 };
